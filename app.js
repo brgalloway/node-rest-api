99766eecffff2a9e58084fe4ce8b95a438a880ad
Billy Galloway <galloway@olympus.F5Net.com>
2018-03-30 12:14:40 -0700

    added morgan and body-parser

--- app.js
+++ app.js
@@ -1,11 +1,34 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
+// Routes handling requests
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+
+app.use((req, res, next) => {
+    const error = new Error('Not Found');
+    //This will trigger a 500
+    //error.status(404);
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
 
 module.exports = app;
