73677aeaed4134aab5865464a6bbdda998d5dcf5
Billy Galloway <galloway@olympus.F5Net.com>
2018-03-30 14:01:16 -0700

    added cors

--- app.js
+++ app.js
@@ -1,26 +1,41 @@
 const bodyParser = require('body-parser');
 const express = require('express');
-const app = express();
 const morgan = require('morgan');
-
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+const app = express();
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+
+//cors header access control
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+        'Access-Control-Allow-Headers', 
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    if (req.method == 'OPTIONS'){
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+});
+
 
 // Routes handling requests
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 
 app.use((req, res, next) => {
-    const error = new Error('Not Found');
+    const error = new Error('Nothing to show here');
     //This will trigger a 500
     //error.status(404);
     error.status = 404;
     next(error);
 });
+
 
 app.use((error, req, res, next) => {
     res.status(error.status || 500);
